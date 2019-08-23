69b7aba8c32828d2b182c5daf6f122486b574114
Ben <dreamvb@outlook.com>
2019-08-23 21:24:00 +0100

    Delete table.sql

--- address/table.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE address (id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), phone VARCHAR(30), email VARCHAR(30), website VARCHAR(30));
-INSERT INTO address (name, phone, email, website) VALUES ( "Ben Jones", "0552 141545", "dreamvb@outlook.com","https://github.com/DreamVB"), ( "John Brown", "01254 457893", "John@nospam.com","http://www.google.com")
