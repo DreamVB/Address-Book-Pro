715b9fb24452d022a7dd31ea028e04bdc9e8a347
Ben <dreamvb@outlook.com>
2019-08-26 21:00:00 +0100

    Add files via upload

--- address/index.php
+++ address/index.php
@@ -19,9 +19,9 @@
 	
 		<div style="overflow-x:auto;">
 		<table>
-		<tr>
-			<td>ID</td>
-			<td>Name</td>
+		<tr class="t_header noselect2">
+			<td>#</td>
+			<td>Company / Name</td>
 			<td>Phone</td>
 			<td>Contact</td>
 			<td>Admin</td>
@@ -36,7 +36,7 @@
     	die("Connection failed: " . $conn->connect_error);
 	}
 	//SQL statement
-	$sql = "SELECT * FROM address";
+	$sql = "SELECT * FROM address ORDER BY name ASC";
 	//Execute SQL statement
 	$result = $conn->query($sql);
 	//Check results
@@ -45,12 +45,12 @@
     	while($row = $result->fetch_assoc()) {
 			$id = $row["id"];
 			$name = $row["name"];
-			echo "<tr><td>". $id ."</td>\n";
+			echo "<tr><td class=bold>". $id ."</td>\n";
 			echo "<td>" .$row["name"] ."</td>\n";
-			echo "<td>" .$row["phone"] ."</td>\n";
+			echo "<td>" . "&#x260E; " .$row["phone"] ."</td>\n";
 			echo "<td><a class=email href=mailto:".$row["email"] .
 			" target=_blank><img class=img-icon title=Email src=images/email.png></a><a href=" .
-			$row["website"] ." target=_blank><img title='Visit Home-Page' class=img-icon src=images/home.png></a></td>\n";
+			$row["website"] ." target=_blank><img title='Visit Home-Page' class=img-icon src=images/home.png></a></td>";
 		
 	//Check if logged
     if((isset($_SESSION['logged']))
@@ -64,10 +64,10 @@
 		if($logged){
 			echo "<td><a class=link href=\"" ."edit.php?id=".$id.
 			"\"" ." onclick=\"" ."return confirm('Are you sure you want to edit this address?')\"".">Edit</a> | <a class=link href=\"" ."delete.php?id=".$id.
-			"\"" ." onclick=\"" ."return confirm('Are you sure want to delete this address?')\"".">Delete</a></td><tr>";
+			"\"" ." onclick=\"" ."return confirm('Are you sure want to delete this address?')\"".">Delete</a></td></tr>";
 		}
 		else{
-			echo "<td class=noselect>Please login to edit</td><tr>";
+			echo "<td class=noselect>Please login to edit</td></tr>";
 		}
 		
     	}
